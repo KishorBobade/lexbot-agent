0314b87dce91c10358d9137bf2d2ee716fff14ef
Kishor Patil <135812528+KishorBobade@users.noreply.github.com>
2026-03-20 17:50:46 +0530

    Add files via upload

---
+++ app/models/llm.py
@@ -0,0 +1,12 @@
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_llm():
+    return ChatGroq(
+        model="llama-3.1-8b-instant",   #  UPDATED MODEL
+        groq_api_key=os.getenv("GROQ_API_KEY"),
+        temperature=0.3
+    )
